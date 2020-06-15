b43d379b4fcfd65cad7aba5a37d076b35bba38dd
Fahd <fahd.addi@gmail.com>
2020-06-15 14:26:38 +0100

    footer component

---
+++ components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.element {
+  transition: all 0.4s ease-in-out;
+}
+.blink {
+  animation: blink-animation 2s steps(5, start) infinite;
+  -webkit-animation: blink-animation 2s steps(5, start) infinite;
+}
+@keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+@-webkit-keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+.router-link-active {
+  @apply opacity-100 text-primary;
+}
+</style>
