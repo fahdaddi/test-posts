e6934b84503b97d0ea566a05d472a649119cec51
Fahd <fahd.addi@gmail.com>
2020-06-15 14:14:37 +0100

    svg-icon component

---
+++ components/COMMON/SVGIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="fill-current"
+    :class="iconStyle"
+    v-html="require(`!svg-inline-loader?removeSVGTagAttrs=false!~/assets/icons/${icon}.svg`)"
+  ></div>
+</template>
+
+<style module>
+.svg {
+	fill: currentColor;
+	/* height: 1em; */
+	/* margin-top: -4px; */
+	vertical-align: middle;
+	/* width: 1em; */
+}
+</style>
+
+<script>
+export default {
+	props: {
+		icon: {
+			type: String,
+			default: 'activity'
+		},
+		iconStyle: {
+			type: String,
+			default: 'w-6 h-6'
+		}
+	},
+	mounted() {
+		// this.$el.firstChild.classList.add(this.$style.svg)
+		this.$el.firstChild.removeAttribute("height");
+		this.$el.firstChild.removeAttribute("width");
+	}
+};
+</script>
