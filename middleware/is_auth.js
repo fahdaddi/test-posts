dde7dc0baea3c001866de154da13cabcce7e3219
Fahd <fahd.addi@gmail.com>
2020-06-15 15:22:13 +0100

    add missing param in middleware

--- middleware/is_auth.js
+++ middleware/is_auth.js
@@ -1,4 +1,4 @@
-export default function({ store, redirect }) {
+export default function({ store, redirect, route }) {
     // If user not connected, redirect to /login
     if (!store.getters["user/IS_AUTH"]) {
         return redirect('/login', { callback: route.fullPath })
